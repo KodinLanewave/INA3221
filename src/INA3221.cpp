b935bd428ac1493e59ee2fccb125ca6bf7a2a3a8
KodinLanewave <kodin.lanewave@gmail.com>
2023-12-13 14:48:05 -0800

    Floating point changes
    I have no idea what I'm doing please help

--- src/INA3221.cpp
+++ src/INA3221.cpp
@@ -395,7 +395,7 @@ void INA3221::setCurrentSumDisable(ina3221_ch_t channel) {
     _masken_reg = masken_reg;
 }
 
-int16_t INA3221::getShuntVoltage(ina3221_ch_t channel) {
+float INA3221::getShuntVoltage(ina3221_ch_t channel) {
     float res;
     ina3221_reg_t reg;
     uint16_t val_raw = 0;
